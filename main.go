ec602616b49a27de0ee677151fc182e9edce5e9f
Yadav106 <sahil.jp.yadav@gmail.com>
2024-11-21 16:01:54 +0530

    added game loop

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 var elapsedTime float64
 var exitChan = make(chan bool)
 
@@ -9,7 +14,31 @@ var settings Settings
 var screen Screen
 
 func main() {
-  gameMap.init()
-  settings.init()
-  screen.init()
+	gameMap.init()
+	settings.init()
+	screen.init()
+
+	go player.move()
+
+	buffer := make([]string, screen.height*screen.width)
+	fmt.Println(buffer)
+
+	fmt.Printf("\x1b[2J\x1b[?25l")
+	defer fmt.Printf("\x1b[?25h")
+
+	time_point_1 := time.Now()
+	for true {
+		time_point_2 := time.Now()
+		elapsedTime = time_point_2.Sub(time_point_1).Seconds()
+		time_point_1 = time_point_2
+
+		select {
+		case <-exitChan:
+			fmt.Println("Exiting game loop...")
+			return
+		default:
+			time.Sleep(settings.sleepTime)
+		}
+	}
+
 }
--- settings.go
+++ settings.go
@@ -1,13 +1,17 @@
 package main
 
+import "time"
+
 type Settings struct {
   FOV float64
   depth float64
   showDetails bool
+  sleepTime time.Duration
 }
 
 func (s *Settings) init() {
   s.FOV = -3.1415 / 4.0
   s.depth = 16
   s.showDetails = false
+  s.sleepTime = time.Microsecond * 8000 * 2
 }
